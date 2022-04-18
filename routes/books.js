85e62f2c735d5cff2b7b70c43df926fef3a52390
Ckbhatia <chetansain86@gmail.com>
2022-04-18 12:58:35 +0530

    feat: create books route

---
+++ routes/books.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const BooksSearch = require("../models/books");
+
+/* POST book search query. */
+router.post('/', async (req, res, next) => {
+  const searchQuery = req.body?.searchQuery;
+  const userId = req.body?.userId;
+
+  try {
+    const bookSearches = await BooksSearch.find()
+      .where('userId').equals(userId).exec();
+
+    let response;
+
+    if (bookSearches?.length > 0) {
+      for (let bookSearch of bookSearches) {
+        if (bookSearch?.searchString === searchQuery) {
+          response = res.status(200).json({ message: "Book search already saved" });
+          break;
+        }
+      }
+
+      // Return response if available
+      if (response) {
+        return response;
+      }
+
+      const newBook = new BooksSearch({
+        searchString: searchQuery,
+        userId: userId,
+      });
+      await newBook.save();
+      return res.status(201).json({ message: "Book search saved" });
+    }
+
+    else {
+      const newBook = new BooksSearch({
+        searchString: searchQuery,
+        userId: userId,
+      });
+      await newBook.save();
+      return res.status(201).json({ message: "Book search saved" });
+
+    }
+  } catch (err) {
+    return res.status(400).json({ message: "Book query save failed" });
+  }
+});
+
+module.exports = router;
